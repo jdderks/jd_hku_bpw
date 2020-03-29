f8cb74d3a67b8c33b5ea65780b059468f49b5bb9
jorisdd <derks893@gmail.com>
2020-03-29 15:57:51 +0200

    add main menu

---
+++ Assets/Scripts/SceneSwitching.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitching : MonoBehaviour
+{
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void CloseApplication()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    private void Update()
+    {
+        //bool isPaused = false;
+
+        if (Input.GetButtonDown("Cancel"))
+        {
+            
+            togglePause();
+        }
+    }
+
+    public void togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
